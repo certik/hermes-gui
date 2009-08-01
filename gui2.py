386b5e2ab260a1afde1bac7ec30544e5db27ecd0
Ondrej Certik <ondrej@certik.cz>
2009-07-31 21:05:07 -0600

    toolbar

--- gui2.py
+++ gui2.py
@@ -3,12 +3,12 @@
 from enthought.traits.api import (Int, HasTraits, Enum, CInt, String, Instance,
         Str)
 from enthought.traits.ui.api import (View, Item, HSplit, VSplit, TreeEditor,
-        TreeNode)
+        TreeNode, ToolBar, Action)
 from enthought.traits.ui.menu import NoButtons
 from enthought.pyface.image_resource import ImageResource
 from enthought.pyface.api import ApplicationWindow, GUI, PythonShell
 from enthought.traits.ui.api import CustomEditor
-from enthought.pyface.action.api import (Action, MenuManager, MenuBarManager,
+from enthought.pyface.action.api import (MenuManager, MenuBarManager,
         StatusBarManager, ToolBarManager, Group, Separator)
 
 from enthought.traits.api import HasTraits, Str, Regex, List, Instance
@@ -16,10 +16,16 @@ from enthought.traits.ui.api import Item, View, TreeEditor, TreeNode
 
 class MainWindow(HasTraits):
     """ The main application window. """
-    pass
 
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.my_action = Action(name="stuff")
+
+    def get_view(self):
+        return View(
+                toolbar = ToolBar(self.my_action))
 
 
 if __name__ == '__main__':
     main_window = MainWindow()
-    main_window.configure_traits()
+    main_window.configure_traits(view=main_window.get_view())
