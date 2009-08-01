79f6cb22c1b73dd0b2779aa8358cd0c571c2a88f
Ondrej Certik <ondrej@certik.cz>
2009-08-01 17:56:58 -0600

    Add some panels

--- acme/core/core_plugin.py
+++ acme/core/core_plugin.py
@@ -4,7 +4,10 @@ from enthought.traits.api import List
 
 class HermesPlugin(Plugin):
     ACTION_SETS       = 'enthought.envisage.ui.workbench.action_sets'
+    PERSPECTIVES      = 'enthought.envisage.ui.workbench.perspectives'
+    PREFERENCES_PAGES = 'enthought.envisage.ui.workbench.preferences_pages'
     VIEWS             = 'enthought.envisage.ui.workbench.views'
+
 
     id = 'acme.core'
     name = 'Core Workbench'
@@ -17,6 +20,15 @@ class HermesPlugin(Plugin):
         from action_set import ActionSet
 
         return [ActionSet]
+
+    perspectives = List(contributes_to=PERSPECTIVES)
+
+    def _perspectives_default(self):
+        """ Trait initializer. """
+
+        from acme.workbench.perspective.api import FooPerspective
+
+        return [FooPerspective]
 
     views = List(contributes_to=VIEWS)
 
