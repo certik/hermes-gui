887e9309c90b9b7cee1ac003998ebd8549f3ebf0
Ondrej Certik <ondrej@certik.cz>
2009-08-02 00:48:57 -0600

    Be quiet

--- acme/core/handle_hermes.py
+++ acme/core/handle_hermes.py
@@ -1,6 +1,6 @@
 from hermes2d import Mesh, H1Shapeset, PrecalcShapeset, H1Space, \
                 WeakForm, Solution, ScalarView, LinSystem, DummySolver, \
-                MeshView
+                MeshView, set_verbose
 from hermes2d.forms import set_forms
 from hermes2d.mesh import read_hermes_format
 from hermes2d.plot import plot_mesh_mpl2
@@ -21,6 +21,7 @@ def poisson_solver(mesh_tuple):
 
     mesh_tuple ... a tuple of (nodes, elements, boundary, nurbs)
     """
+    set_verbose(False)
     mesh = Mesh()
     mesh.create(*mesh_tuple)
     mesh.refine_element(0)
