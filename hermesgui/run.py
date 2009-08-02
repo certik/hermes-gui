9a423796bbb2c83eb7f09eebec32d82aabc11bc9
Ondrej Certik <ondrej@certik.cz>
2009-08-02 15:41:09 -0600

    Move the startup logic into hermesgui/run.py

---
+++ hermesgui/run.py
@@ -0,0 +1,49 @@
+# delete the configuration, so that we can easily develop without any "caching"
+# effects:
+import os
+os.system("rm -rf ~/.enthought/acme.acmelab/")
+
+# Standard library imports.
+import logging
+
+# Example imports.
+from hermesgui.acmelab.api import Acmelab
+
+# Enthought plugins.
+from enthought.envisage.core_plugin import CorePlugin
+from enthought.envisage.developer.developer_plugin import DeveloperPlugin
+from enthought.envisage.developer.ui.developer_ui_plugin import DeveloperUIPlugin
+from enthought.envisage.ui.workbench.workbench_plugin import WorkbenchPlugin
+
+from hermesgui.core.core_plugin import HermesPlugin
+
+
+# Do whatever you want to do with log messages! Here we create a log file.
+logger = logging.getLogger()
+#logger.addHandler(logging.StreamHandler(file('acmelab.log', 'w')))
+logger.addHandler(logging.StreamHandler())
+#logger.setLevel(logging.DEBUG)
+
+
+def main():
+    """ Run the application. """
+
+    # Create an application with the specified plugins.
+    acmelab = Acmelab(
+        plugins=[
+            CorePlugin(),
+            WorkbenchPlugin(),
+            #AcmeWorkbenchPlugin(),
+            HermesPlugin(),
+            #DeveloperPlugin(),
+            #DeveloperUIPlugin()
+        ]
+    )
+
+    acmelab.workbench.prompt_on_exit = False
+
+    # Run it! This starts the application, starts the GUI event loop, and when
+    # that terminates, stops the application.
+    acmelab.run()
+
+    return
