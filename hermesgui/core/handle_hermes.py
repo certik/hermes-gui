b582da8d245b6fb582d12f7221021c687f73e071
Ondrej Certik <ondrej@certik.cz>
2009-08-03 23:12:31 -0600

    Update to the latest hermes

--- hermesgui/core/handle_hermes.py
+++ hermesgui/core/handle_hermes.py
@@ -1,9 +1,8 @@
 from hermes2d import Mesh, H1Shapeset, PrecalcShapeset, H1Space, \
                 WeakForm, Solution, ScalarView, LinSystem, DummySolver, \
-                MeshView, set_verbose
+                MeshView, set_verbose, plot_mesh_mpl_simple
 from hermes2d.forms import set_forms
 from hermes2d.mesh import read_hermes_format
-from hermes2d.plot import plot_mesh_mpl2
 
 def read_mesh(filename):
     nodes, elements, boundary, nurbs = read_hermes_format(filename)
@@ -13,7 +12,8 @@ def plot_mesh(mesh, axes=None, plot_nodes=True):
     nodes, elements, boundary, nurbs = mesh
     # remove the element markers
     elements = [x[:-1] for x in elements]
-    return plot_mesh_mpl2(nodes, elements, axes=axes, plot_nodes=plot_nodes)
+    return plot_mesh_mpl_simple(nodes, elements, axes=axes,
+            plot_nodes=plot_nodes)
 
 def poisson_solver(mesh_tuple):
     """
